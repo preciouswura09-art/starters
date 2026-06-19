f128021891187f7eb80111116ba1d4cb59a246e1
preciouswura09-art <preciouswura09@gmail.com>
2026-06-19 13:13:41 +0100

    changes done

--- src/Features2.jsx
+++ src/Features2.jsx
@@ -5,7 +5,7 @@ import { house } from "./constants/house"
 const Features2 = () => {
   return (
     <div>
-        <h2>features</h2>
+        <h5>features</h5>
     <div className='card-con2'>
         {house.map((data, i)=>
         <Cards2 Card2Details={data} key={i}/>
